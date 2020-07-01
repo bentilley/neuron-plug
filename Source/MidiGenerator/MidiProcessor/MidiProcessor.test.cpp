57c8f2139ee14a6049b3c04560352ebe7f8a8bf1
bentilley <tilleyben@hotmail.com>
2020-07-01 16:54:04 +0100

    Tests - rewrite MidiProcessor tests using Catch

--- Source/MidiGenerator/MidiProcessor/MidiProcessor.test.cpp
+++ Source/MidiGenerator/MidiProcessor/MidiProcessor.test.cpp
@@ -6,204 +6,207 @@
  */
 
 #include "MidiProcessor.hpp"
+#include <catch2/catch.hpp>
 
-class MidiProcessorTests : public UnitTest {
-public:
-  MidiProcessorTests() : UnitTest("MidiProcessor Testing") {}
-
-  void runTest() override {
+SCENARIO("MidiProcessor") {
+  GIVEN("an instance of MidiProcessor, neurons: 3") {
     MidiProcessor processor(3);
 
-    // == Instantiation ==
-    beginTest("Instantiation");
+    // Test default values
+    REQUIRE(processor.get_midi_map() == std::vector<int>{1, 1, 1});
+    REQUIRE(processor.get_global_volume() == Approx(1.0f));
 
-    expect(processor.get_midi_map() == std::vector<int>{1, 1, 1},
-           "MidiProcessor missing default `midi_map`");
+    REQUIRE(processor.get_volume_clip_min() == 1);
+    REQUIRE(processor.get_volume_clip_max() == 127);
+    REQUIRE(processor.get_volume_clip_range() == 126);
 
-    // == Getters & Setters
-    beginTest("global volume");
+    REQUIRE(processor.get_note_at(0) == 1);
+    REQUIRE(processor.get_note_at(1) == 1);
+    REQUIRE(processor.get_note_at(2) == 1);
 
-    expectWithinAbsoluteError<float>(processor.get_global_volume(), 1.0f, 0.01f,
-                                     "default global volume is wrong");
-    processor.set_global_volume(0.5f);
-    expectWithinAbsoluteError<float>(processor.get_global_volume(), 0.5f, 0.01f,
-                                     "global volume was not set correctly");
-    processor.set_global_volume(0.69f);
-    expectWithinAbsoluteError<float>(processor.get_global_volume(), 0.69f,
-                                     0.01f,
-                                     "global volume was not set correctly");
-    processor.set_global_volume(1.0f);
+    WHEN("we set the global volume") {
+      processor.set_global_volume(0.69f);
 
-    beginTest("volume clip range");
+      REQUIRE(processor.get_global_volume() == Approx(0.69f));
+    }
 
-    expect(processor.get_volume_clip_min() == 1, "wrong default vol clip");
-    expect(processor.get_volume_clip_max() == 127, "wrong default vol clip");
-    expect(processor.get_volume_clip_range() == 126, "wrong default vol clip");
+    WHEN("we set the volume clip range") {
+      processor.set_volume_clip(52, 100);
 
-    processor.set_volume_clip(52, 100);
-    expect(processor.get_volume_clip_min() == 52, "wrong vol clip set");
-    expect(processor.get_volume_clip_max() == 100, "wrong vol clip set");
-    expect(processor.get_volume_clip_range() == 48, "wrong vol clip set");
-    processor.set_volume_clip(1, 127);
+      REQUIRE(processor.get_volume_clip_min() == 52);
+      REQUIRE(processor.get_volume_clip_max() == 100);
+      REQUIRE(processor.get_volume_clip_range() == 48);
+    }
 
-    // == MIDI Notes ==
-    beginTest("MIDI Notes");
+    WHEN("we set the MIDI notes") {
+      processor.set_note_at(0, 25);
+      processor.set_note_at(1, 89);
+      processor.set_note_at(2, 111);
 
-    expect(processor.get_note_at(0) == 1, "1st default note is 1");
-    expect(processor.get_note_at(1) == 1, "2nd default note is 1");
-    expect(processor.get_note_at(2) == 1, "3rd default note is 1");
+      REQUIRE(processor.get_note_at(0) == 25);
+      REQUIRE(processor.get_note_at(1) == 89);
+      REQUIRE(processor.get_note_at(2) == 111);
+    }
+  }
+}
 
-    processor.set_note_at(0, 25);
-    processor.set_note_at(1, 89);
-    processor.set_note_at(2, 111);
+SCENARIO("MidiProcessor MIDI Volume") {
+  GIVEN("an instance of MidiProcessor, neurons: 3") {
 
-    expect(processor.get_note_at(0) == 25, "new note is 25");
-    expect(processor.get_note_at(1) == 89, "new note is 89");
-    expect(processor.get_note_at(2) == 111, "new note is 111");
+    MidiProcessor processor(3);
 
-    // == Clip Midi Volume ==
-    beginTest("clip_brain_output");
+    WHEN("we clip the brain output") {
+      REQUIRE(processor.clip_brain_output(0) == 0);
+      REQUIRE(processor.clip_brain_output(1) == 1);
+      REQUIRE(processor.clip_brain_output(2) == 1);
+    }
 
-    expect(processor.clip_brain_output(0) == 0, "wrong clipped brain output 0");
-    expect(processor.clip_brain_output(1) == 1, "wrong clipped brain output 1");
-    expect(processor.clip_brain_output(2) == 1, "wrong clipped brain output 2");
+    WHEN("we get the volume as a percentage of the max volume") {
+      REQUIRE(processor.as_percent_of_max(0) == Approx(0.0f));
+      REQUIRE(processor.as_percent_of_max(1) == Approx(1.0f));
+    }
 
-    beginTest("as_percent_of_max");
+    WHEN("we clip the MIDI volume, clip: (1, 127)") {
+      REQUIRE(processor.get_clipped_midi_volume(1.0f) == 127);
+      REQUIRE(processor.get_clipped_midi_volume(0.8f) == 102);
+      REQUIRE(processor.get_clipped_midi_volume(0.0f) == 0);
+    }
 
-    expectWithinAbsoluteError<float>(processor.as_percent_of_max(0), 0.0f,
-                                     0.01f,
-                                     "incorrect output percentage for 0");
-    expectWithinAbsoluteError<float>(processor.as_percent_of_max(1), 1.0f,
-                                     0.01f,
-                                     "incorrect output percentage for 1");
+    WHEN("we clip the MIDI volume, clip: (50, 100)") {
+      processor.set_volume_clip(50, 100);
+      REQUIRE(processor.get_clipped_midi_volume(1.0f) == 100);
+      REQUIRE(processor.get_clipped_midi_volume(0.8f) == 90);
+      REQUIRE(processor.get_clipped_midi_volume(0.0f) == 0);
+    }
 
-    beginTest("get_clipped_midi_volume");
+    WHEN("we get the note velocity, clip: (1, 127)") {
+      REQUIRE(processor.get_note_velocity(0) == 0);
+      REQUIRE(processor.get_note_velocity(1) == 127);
+    }
 
-    expect(processor.get_clipped_midi_volume(1.0f) == 127, "vol should 127");
-    expect(processor.get_clipped_midi_volume(0.8f) == 102, "vol should 102");
-    expect(processor.get_clipped_midi_volume(0.0f) == 0, "vol should 0");
+    WHEN("we get the note velocity, clip: (50, 80)") {
+      processor.set_volume_clip(50, 80);
 
-    processor.set_volume_clip(50, 100);
-    expect(processor.get_clipped_midi_volume(1.0f) == 100, "vol should 100");
-    expect(processor.get_clipped_midi_volume(0.8f) == 90, "vol should 90");
-    expect(processor.get_clipped_midi_volume(0.0f) == 0, "vol should 0");
-    processor.set_volume_clip(1, 127);
+      REQUIRE(processor.get_note_velocity(0) == 0);
+      REQUIRE(processor.get_note_velocity(1) == 80);
+    }
 
-    beginTest("get_note_velocity");
+    WHEN("we get the note velocity, clip: (50, 80), global volume: 0.5") {
+      processor.set_volume_clip(50, 80);
+      processor.set_global_volume(0.5f);
 
-    expect(processor.get_note_velocity(0) == 0, "note vel should be 0");
-    expect(processor.get_note_velocity(1) == 127, "note vel should be 127");
+      REQUIRE(processor.get_note_velocity(0) == 0);
+      REQUIRE(processor.get_note_velocity(1) == 40);
+    }
+  }
+}
 
-    processor.set_volume_clip(50, 80);
-    expect(processor.get_note_velocity(0) == 0, "note vel should be 0");
-    expect(processor.get_note_velocity(1) == 80, "note vel should be 80");
+SCENARIO("MidiProcessor Adding and Removing Notes") {
+  GIVEN("an instance of MidiProcessor, neurons: 3") {
+    MidiProcessor processor(3);
 
-    processor.set_global_volume(0.5f);
-    expect(processor.get_note_velocity(0) == 0, "note vel should be 0");
-    expect(processor.get_note_velocity(1) == 40, "note vel should be 40");
+    WHEN("we set the note values") {
+      processor.set_note_at(0, 60);
+      processor.set_note_at(1, 64);
+      processor.set_note_at(2, 67);
 
-    processor.set_global_volume(1.0f);
-    processor.set_volume_clip(1, 127);
+      REQUIRE(processor.get_midi_map() == std::vector<int>{60, 64, 67});
 
-    // == add_midi_note, remove_midi_note, remove_midi_note_at ==
-    beginTest("add_midi_note");
+      THEN("we add two notes") {
+        processor.add_midi_note(60);
+        REQUIRE(processor.get_midi_map() == std::vector<int>{60, 64, 67, 60});
 
-    processor.set_note_at(0, 60);
-    processor.set_note_at(1, 64);
-    processor.set_note_at(2, 67);
+        processor.add_midi_note(8);
+        REQUIRE(processor.get_midi_map() ==
+                std::vector<int>{60, 64, 67, 60, 8});
 
-    expect(processor.get_midi_map() == std::vector<int>{60, 64, 67});
+        THEN("we remove three notes") {
+          processor.remove_midi_note();
+          REQUIRE(processor.get_midi_map() == std::vector<int>{60, 64, 67, 60});
 
-    processor.add_midi_note(60);
-    expect(processor.get_midi_map() == std::vector<int>{60, 64, 67, 60});
+          processor.remove_midi_note_at(0);
+          REQUIRE(processor.get_midi_map() == std::vector<int>{64, 67, 60});
 
-    processor.add_midi_note(8);
-    expect(processor.get_midi_map() == std::vector<int>{60, 64, 67, 60, 8});
+          processor.remove_midi_note_at(1);
+          REQUIRE(processor.get_midi_map() == std::vector<int>{64, 60});
+        }
+      }
+    }
+  }
+}
 
-    processor.remove_midi_note();
-    expect(processor.get_midi_map() == std::vector<int>{60, 64, 67, 60});
-
-    processor.remove_midi_note_at(0);
-    expect(processor.get_midi_map() == std::vector<int>{64, 67, 60});
-
-    processor.remove_midi_note_at(1);
-    expect(processor.get_midi_map() == std::vector<int>{64, 60});
-
-    processor.add_midi_note();
-    expect(processor.get_midi_map() == std::vector<int>{64, 60, 1});
-
-    // == render_buffer ==
-    beginTest("render_buffer");
-
-    processor.set_note_at(0, 60);
-    processor.set_note_at(1, 64);
-    processor.set_note_at(2, 67);
-
-    std::vector<int> output{1, 1, 1};
+SCENARIO("MidiProcessor Buffer Rendering") {
+  GIVEN("an instance of MidiProcessor, neurons: 3") {
+    MidiProcessor processor(3);
     MidiBuffer buffer;
-    int sample_number = 100;
-    processor.render_buffer(buffer, output, sample_number);
+    processor.set_note_at(0, 60);
+    processor.set_note_at(1, 64);
+    processor.set_note_at(2, 67);
 
-    expect(buffer.getNumEvents() == 3, "Wrong number of MIDI events");
-    expect(buffer.getFirstEventTime() == 100,
-           "MIDI events have wrong sample number");
+    WHEN("we render a buffer, output: (1, 1, 1), sample_number: 100") {
+      std::vector<int> output = std::vector<int>{1, 1, 1};
+      int sample_number = 100;
+      processor.render_buffer(buffer, output, sample_number);
 
-    output = std::vector<int>{1, 0, 1};
-    buffer = MidiBuffer();
-    sample_number = 167;
-    processor.render_buffer(buffer, output, sample_number);
-
-    expect(buffer.getNumEvents() == 2, "Wrong number of MIDI events");
-    expect(buffer.getFirstEventTime() == 167,
-           "MIDI events have wrong sample number");
-
-    // == render_buffer note numbers==
-    beginTest("render_buffer note numbers");
-
-    output = std::vector<int>{1, 0, 0};
-    buffer = MidiBuffer();
-    sample_number = 17;
-    processor.render_buffer(buffer, output, sample_number);
-
-    expect(buffer.getNumEvents() == 1, "Wrong number of MIDI events");
-    expect(buffer.getFirstEventTime() == 17,
-           "MIDI events have wrong sample number");
-
-    int time;
-    MidiMessage m;
-    for (MidiBuffer::Iterator i(buffer); i.getNextEvent(m, time);) {
-      expect(m.getNoteNumber() == 60, "note number is incorrect");
-      expect(m.getVelocity() == 127, "note velocity is incorrect");
+      REQUIRE(buffer.getNumEvents() == 3);
+      REQUIRE(buffer.getFirstEventTime() == 100);
     }
 
-    output = std::vector<int>{0, 1, 0};
-    buffer = MidiBuffer();
-    sample_number = 45;
-    processor.render_buffer(buffer, output, sample_number);
+    WHEN("we render a buffer, output: (1, 0, 1), sample_number: 167") {
+      std::vector<int> output = std::vector<int>{1, 0, 1};
+      int sample_number = 167;
+      processor.render_buffer(buffer, output, sample_number);
 
-    expect(buffer.getNumEvents() == 1, "Wrong number of MIDI events");
-    expect(buffer.getFirstEventTime() == 45,
-           "MIDI events have wrong sample number");
-
-    for (MidiBuffer::Iterator i(buffer); i.getNextEvent(m, time);) {
-      expect(m.getNoteNumber() == 64, "note number is incorrect");
-      expect(m.getVelocity() == 127, "note velocity is incorrect");
+      REQUIRE(buffer.getNumEvents() == 2);
+      REQUIRE(buffer.getFirstEventTime() == 167);
     }
 
-    output = std::vector<int>{0, 0, 1};
-    buffer = MidiBuffer();
-    sample_number = 229;
-    processor.render_buffer(buffer, output, sample_number);
+    WHEN("we render a buffer, output: (1, 0, 0), sample_number: 17") {
+      std::vector<int> output = std::vector<int>{1, 0, 0};
+      int sample_number = 17;
+      processor.render_buffer(buffer, output, sample_number);
 
-    expect(buffer.getNumEvents() == 1, "Wrong number of MIDI events");
-    expect(buffer.getFirstEventTime() == 229,
-           "MIDI events have wrong sample number");
+      REQUIRE(buffer.getNumEvents() == 1);
+      REQUIRE(buffer.getFirstEventTime() == 17);
 
-    for (MidiBuffer::Iterator i(buffer); i.getNextEvent(m, time);) {
-      expect(m.getNoteNumber() == 67, "note number is incorrect");
-      expect(m.getVelocity() == 127, "note velocity is incorrect");
+      int time;
+      MidiMessage m;
+      for (MidiBuffer::Iterator i(buffer); i.getNextEvent(m, time);) {
+        REQUIRE(m.getNoteNumber() == 60);
+        REQUIRE(m.getVelocity() == 127);
+      }
     }
-  };
-};
 
-static MidiProcessorTests test;
+    WHEN("we render a buffer, output: (0, 1, 0), sample_number: 45") {
+      std::vector<int> output = std::vector<int>{0, 1, 0};
+      int sample_number = 45;
+      processor.render_buffer(buffer, output, sample_number);
+
+      REQUIRE(buffer.getNumEvents() == 1);
+      REQUIRE(buffer.getFirstEventTime() == 45);
+
+      int time;
+      MidiMessage m;
+      for (MidiBuffer::Iterator i(buffer); i.getNextEvent(m, time);) {
+        REQUIRE(m.getNoteNumber() == 64);
+        REQUIRE(m.getVelocity() == 127);
+      }
+    }
+
+    WHEN("we render a buffer, output: (0, 0, 1), sample_number: 229") {
+      std::vector<int> output = std::vector<int>{0, 0, 1};
+      int sample_number = 229;
+      processor.render_buffer(buffer, output, sample_number);
+
+      REQUIRE(buffer.getNumEvents() == 1);
+      REQUIRE(buffer.getFirstEventTime() == 229);
+
+      int time;
+      MidiMessage m;
+      for (MidiBuffer::Iterator i(buffer); i.getNextEvent(m, time);) {
+        REQUIRE(m.getNoteNumber() == 67);
+        REQUIRE(m.getVelocity() == 127);
+      }
+    }
+  }
+}
