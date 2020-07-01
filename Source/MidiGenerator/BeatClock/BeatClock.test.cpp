262c54afcb7202ee7af4a0f484dd79cdf446c245
bentilley <tilleyben@hotmail.com>
2020-07-01 16:53:40 +0100

    Tests - rewrite BeatClock JUCE tests using Catch

--- Source/MidiGenerator/BeatClock/BeatClock.test.cpp
+++ Source/MidiGenerator/BeatClock/BeatClock.test.cpp
@@ -6,133 +6,166 @@
  */
 
 #include "BeatClock.hpp"
+#include <catch2/catch.hpp>
 
-class BeatClockTests : public UnitTest {
-public:
-  BeatClockTests() : UnitTest("BeatClock Testing") {}
-
-  void runTest() override {
+SCENARIO("BeatClock") {
+  GIVEN("an instance of a BeatClock") {
     BeatClock clock;
 
-    // == Instantiation ==
-    beginTest("Instantiation");
+    REQUIRE(!clock.is_configured());
+    REQUIRE(clock.get_subdivision() == 1);
 
-    expect(!clock.is_configured(), "Clock is prematurely configured");
-    expect(clock.get_subdivision() == 1, "Clock has wrong default subdivision");
+    WHEN("we configure the BeatClock") {
+      double sample_rate = 44100;
+      AudioPlayHead::CurrentPositionInfo pos;
+      pos.bpm = 120;
+      pos.timeInSamples = 905984;
+      clock.configure(sample_rate, pos);
 
-    // == configure ==
-    beginTest("configure");
+      REQUIRE(clock.is_configured());
 
+      THEN("reset the clock") {
+        clock.reset();
+
+        REQUIRE(!clock.is_configured());
+      }
+    }
+  }
+}
+
+SCENARIO("BeatClock Configuration") {
+  GIVEN("an instance of BeatClock") {
+    BeatClock clock;
     double sample_rate = 44100;
     AudioPlayHead::CurrentPositionInfo pos;
 
-    pos.bpm = 120;
-    pos.timeInSamples = 905984;
-    clock.configure(sample_rate, pos);
-    expectWithinAbsoluteError<float>(clock.get_sample_num_remainder(), 1934.0,
-                                     0.01,
-                                     "current_sample_remainder is not correct");
-    expectWithinAbsoluteError<float>(clock.get_samples_per_subdivision(),
-                                     22050.0, 0.01,
-                                     "samples_per_subdivision is not correct");
+    WHEN("BPM: 120, timeInSamples: 905984") {
+      pos.bpm = 120;
+      pos.timeInSamples = 905984;
+      clock.configure(sample_rate, pos);
 
-    pos.bpm = 100;
-    pos.timeInSamples = 4905984;
-    clock.configure(sample_rate, pos);
-    expectWithinAbsoluteError<float>(clock.get_sample_num_remainder(), 10884.0,
-                                     0.01,
-                                     "current_sample_remainder is not correct");
-    expectWithinAbsoluteError<float>(clock.get_samples_per_subdivision(),
-                                     26460.0, 0.01,
-                                     "samples_per_subdivision is not correct");
+      REQUIRE(clock.get_sample_num_remainder() == Approx(1934.0));
+      REQUIRE(clock.get_samples_per_subdivision() == Approx(22050.0));
+    }
 
-    pos.bpm = 149;
-    pos.timeInSamples = 1706784;
-    clock.configure(sample_rate, pos);
-    expectWithinAbsoluteError<float>(clock.get_sample_num_remainder(), 1978.69,
-                                     0.01,
-                                     "current_sample_remainder is not correct");
-    expectWithinAbsoluteError<float>(clock.get_samples_per_subdivision(),
-                                     17758.39, 0.01,
-                                     "samples_per_subdivision is not correct");
+    WHEN("BPM: 100, timeInSamples: 4905984") {
+      pos.bpm = 100;
+      pos.timeInSamples = 4905984;
+      clock.configure(sample_rate, pos);
 
-    clock.set_subdivision(2);
-    pos.bpm = 69;
-    pos.timeInSamples = 642684;
-    clock.configure(sample_rate, pos);
-    expectWithinAbsoluteError<float>(clock.get_sample_num_remainder(), 9944.90,
-                                     0.01,
-                                     "current_sample_remainder is not correct");
-    expectWithinAbsoluteError<float>(clock.get_samples_per_subdivision(),
-                                     19173.91, 0.01,
-                                     "samples_per_subdivision is not correct");
+      REQUIRE(clock.get_sample_num_remainder() == Approx(10884.0));
+      REQUIRE(clock.get_samples_per_subdivision() == Approx(26460.0));
+    }
 
-    clock.set_subdivision(5);
-    pos.bpm = 111;
-    pos.timeInSamples = 34701294;
-    clock.configure(sample_rate, pos);
-    expectWithinAbsoluteError<float>(clock.get_sample_num_remainder(), 2938.59,
-                                     0.01,
-                                     "current_sample_remainder is not correct");
-    expectWithinAbsoluteError<float>(clock.get_samples_per_subdivision(),
-                                     4767.57, 0.01,
-                                     "samples_per_subdivision is not correct");
+    WHEN("BPM: 149, timeInSamples: 1706784") {
+      pos.bpm = 149;
+      pos.timeInSamples = 1706784;
+      clock.configure(sample_rate, pos);
 
-    // == should_play ==
-    beginTest("should_play");
+      REQUIRE(clock.get_sample_num_remainder() == Approx(1978.69));
+      REQUIRE(clock.get_samples_per_subdivision() == Approx(17758.39));
+    }
 
-    pos.bpm = 120;
-    pos.timeInSamples = 905984;
-    clock.configure(sample_rate, pos);
-    expect(!clock.should_play(20114), "clock shouldn't play on sample 20114");
-    expect(!clock.should_play(20115), "clock shouldn't play on sample 20115");
-    expect(clock.should_play(20116), "clock should play on sample 20116");
-    expect(!clock.should_play(20117), "clock shouldn't play on sample 20117");
-    expect(!clock.should_play(20118), "clock shouldn't play on sample 20118");
+    GIVEN("subdivision is set to 2") {
+      clock.set_subdivision(2);
 
-    pos.bpm = 100;
-    pos.timeInSamples = 4905984;
-    clock.configure(sample_rate, pos);
-    expect(!clock.should_play(15574), "clock shouldn't play on sample 15574");
-    expect(!clock.should_play(15575), "clock shouldn't play on sample 15575");
-    expect(clock.should_play(15576), "clock should play on sample 15576");
-    expect(!clock.should_play(15577), "clock shouldn't play on sample 15577");
-    expect(!clock.should_play(15578), "clock shouldn't play on sample 15578");
+      WHEN("BPM: 69, timeInSamples: 642684") {
+        pos.bpm = 69;
+        pos.timeInSamples = 642684;
+        clock.configure(sample_rate, pos);
 
-    pos.bpm = 149;
-    pos.timeInSamples = 1706784;
-    clock.configure(sample_rate, pos);
-    expect(!clock.should_play(15778), "clock shouldn't play on sample 15778");
-    expect(!clock.should_play(15779), "clock shouldn't play on sample 15779");
-    expect(clock.should_play(15780), "clock should play on sample 15780");
-    expect(!clock.should_play(15781), "clock shouldn't play on sample 15781");
-    expect(!clock.should_play(15782), "clock shouldn't play on sample 15782");
+        REQUIRE(clock.get_sample_num_remainder() == Approx(9944.90));
+        REQUIRE(clock.get_samples_per_subdivision() == Approx(19173.91));
+      }
+    }
 
-    clock.set_subdivision(2);
-    pos.bpm = 69;
-    pos.timeInSamples = 642684;
-    clock.configure(sample_rate, pos);
-    expect(!clock.should_play(9228), "clock shouldn't play on sample 9228");
-    expect(!clock.should_play(9229), "clock shouldn't play on sample 9229");
-    expect(clock.should_play(9230), "clock should play on sample 9230");
-    expect(!clock.should_play(9231), "clock shouldn't play on sample 9231");
-    expect(!clock.should_play(9232), "clock shouldn't play on sample 9232");
+    GIVEN("subdivision is set to 5") {
+      clock.set_subdivision(5);
 
-    clock.set_subdivision(5);
-    pos.bpm = 111;
-    pos.timeInSamples = 34701294;
-    clock.configure(sample_rate, pos);
-    expect(!clock.should_play(1827), "clock shouldn't play on sample 1827");
-    expect(!clock.should_play(1828), "clock shouldn't play on sample 1828");
-    expect(clock.should_play(1829), "clock should play on sample 1829");
-    expect(!clock.should_play(1830), "clock shouldn't play on sample 1830");
-    expect(!clock.should_play(1831), "clock shouldn't play on sample 1831");
+      WHEN("BPM: 111, timeInSamples: 34701294") {
+        pos.bpm = 111;
+        pos.timeInSamples = 34701294;
+        clock.configure(sample_rate, pos);
 
-    // == reset ==
-    beginTest("reset");
-    clock.reset();
-    expect(!clock.is_configured());
-  };
-};
+        REQUIRE(clock.get_sample_num_remainder() == Approx(2938.59));
+        REQUIRE(clock.get_samples_per_subdivision() == Approx(4767.57));
+      }
+    }
+  }
+}
 
-static BeatClockTests test;
+SCENARIO("BeatClock Should Play") {
+  GIVEN("an instance of BeatClock") {
+    BeatClock clock;
+    double sample_rate = 44100;
+    AudioPlayHead::CurrentPositionInfo pos;
+
+    WHEN("BPM: 120, timeInSamples: 905984") {
+      pos.bpm = 120;
+      pos.timeInSamples = 905984;
+      clock.configure(sample_rate, pos);
+
+      REQUIRE(!clock.should_play(20114));
+      REQUIRE(!clock.should_play(20115));
+      REQUIRE(clock.should_play(20116));
+      REQUIRE(!clock.should_play(20117));
+      REQUIRE(!clock.should_play(20118));
+    }
+
+    WHEN("BPM: 100, timeInSamples: 4905984") {
+      pos.bpm = 100;
+      pos.timeInSamples = 4905984;
+      clock.configure(sample_rate, pos);
+
+      REQUIRE(!clock.should_play(15574));
+      REQUIRE(!clock.should_play(15575));
+      REQUIRE(clock.should_play(15576));
+      REQUIRE(!clock.should_play(15577));
+      REQUIRE(!clock.should_play(15578));
+    }
+
+    WHEN("BPM: 149, timeInSamples: 1706784") {
+      pos.bpm = 149;
+      pos.timeInSamples = 1706784;
+      clock.configure(sample_rate, pos);
+
+      REQUIRE(!clock.should_play(15778));
+      REQUIRE(!clock.should_play(15779));
+      REQUIRE(clock.should_play(15780));
+      REQUIRE(!clock.should_play(15781));
+      REQUIRE(!clock.should_play(15782));
+    }
+
+    GIVEN("subdivision is set to 2") {
+      clock.set_subdivision(2);
+
+      WHEN("BPM: 69, timeInSamples: 642684") {
+        pos.bpm = 69;
+        pos.timeInSamples = 642684;
+        clock.configure(sample_rate, pos);
+
+        REQUIRE(!clock.should_play(9228));
+        REQUIRE(!clock.should_play(9229));
+        REQUIRE(clock.should_play(9230));
+        REQUIRE(!clock.should_play(9231));
+        REQUIRE(!clock.should_play(9232));
+      }
+    }
+
+    GIVEN("subdivision is set to 5") {
+      clock.set_subdivision(5);
+
+      WHEN("BPM: 111, timeInSamples: 34701294") {
+        pos.bpm = 111;
+        pos.timeInSamples = 34701294;
+        clock.configure(sample_rate, pos);
+
+        REQUIRE(!clock.should_play(1827));
+        REQUIRE(!clock.should_play(1828));
+        REQUIRE(clock.should_play(1829));
+        REQUIRE(!clock.should_play(1830));
+        REQUIRE(!clock.should_play(1831));
+      }
+    }
+  }
+}
